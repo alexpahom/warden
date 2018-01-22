6bfbea85bbacd6cf64fa3d545df2eb3b18761825
alexpahom <alex.pahomchik@gmail.com>
2018-01-22 13:29:25 +0300

    controller tests

--- test/models/suite_test.rb
+++ test/models/suite_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class SuiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @suite = Suite.new(
+      title: 'Interface Tests',
+      description: 'Here are some words about the suite'
+    )
+  end
+
+  test 'Suite can be created' do
+    assert @suite.valid?
+  end
+
+  test 'Title cannot be blank' do
+    @suite.title = ''
+    assert_not @suite.valid?
+  end
+
+  test 'Title should be unique' do
+    @suite.title = suites(:one).title
+    assert_not @suite.valid?
+  end
 end
