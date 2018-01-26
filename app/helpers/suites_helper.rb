a26f89e4b3121ad7c56838fa773e91b4d2ceafa9
alexpahom <alex.pahomchik@gmail.com>
2018-01-26 18:54:40 +0300

    neat slide down

--- app/helpers/suites_helper.rb
+++ app/helpers/suites_helper.rb
@@ -11,17 +11,21 @@ module SuitesHelper
   end
 
   def render_cases(roots)
-    return ''.html_safe if roots.empty?
+    return ''.html_safe if roots.blank?
     content_tag(:ul) do
       roots.map do |root|
         content_tag(:li) do
-          content_tag(:div, nil, class: 'section-block') do
-            content_tag(:span, root.title, class: 'section-name') +
-                render('suites/section-header', locals: { parent: root.id }) +
-                  render_cases(root.children)
-          end
+          render_block root
         end
       end.join.html_safe
+    end
+  end
+
+  def render_block(section)
+    content_tag(:div, nil, class: 'section-block') do
+      content_tag(:span, section.title, class: 'section-name') +
+          render('suites/section-header', locals: { parent: section.id }) +
+            render_cases(section.children)
     end
   end
 end
